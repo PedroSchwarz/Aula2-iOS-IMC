9d4ba253abdcb11a9c468b51fab2ec460a7eb52b
IOS SENAC <iossenac@fpal152958.local>
2020-07-18 16:14:33 -0300

    Initial Commit

---
+++ Aula2-IMC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Aula2-IMC
+//
+//  Created by IOS SENAC on 18/07/20.
+//  Copyright © 2020 IOS SENAC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
